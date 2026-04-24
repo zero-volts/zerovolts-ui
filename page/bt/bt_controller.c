2f3ec5b0ed9ffc3e76f2b24c998b2d4a92af7cf7
zero-volts <zero.volts.lab@gmail.com>
2026-04-24 15:15:55 -0400

    agrefando un pseudo contexto para mantener datos entre pantallas

--- page/bt/bt_controller.c
+++ page/bt/bt_controller.c
@@ -8,19 +8,23 @@
 #define UART_BAUDRATE 115200
 #define UART_BT_TAG_ID "BT_TAG_CONTROLLER"
 
-// #define MAX_DEVICES 5
-// device_t devices[MAX_DEVICES];
-static bt_event_cb internl_cb = NULL;
+#define MAX_DEVICES 20
+#define DEFAULT_NAME "Unknown"
+static bt_event_cb internal_cb = NULL;
 
 struct bt_context_t {
+    device_t devices[MAX_DEVICES];
+    int current_device_amount;
     device_t selected;
-};
 
+    device_t *visible_devices[MAX_DEVICES];
+    int visible_device_amount;
+};
 static bt_context_t context;
 
 void set_scanner_cb(bt_event_cb new_callback)
 {
-    internl_cb = new_callback;
+    internal_cb = new_callback;
 }
 
 static void event_handler(const char *tag_id, char *buffer)
@@ -29,19 +33,19 @@ static void event_handler(const char *tag_id, char *buffer)
         return;
     }
 
-    if (strstr(buffer, "SCAN:START") != NULL) 
+    if (strstr(buffer, "SCAN:START") != NULL)
     {
-        internl_cb("SCAN:START", NULL);
+        internal_cb("SCAN:START", NULL);
         return;
     }
 
-    if (strstr(buffer, "SCAN:DONE") != NULL) 
+    if (strstr(buffer, "SCAN:DONE") != NULL)
     {
-        internl_cb("SCAN:DONE", NULL);
+        internal_cb("SCAN:DONE", NULL);
         return;
     }
 
-    log_info("buffer handler: %s\n", buffer);
+    // log_info("buffer handler: %s\n", buffer);
     if (strstr(buffer, "SCAN:DEVICE") == NULL) {
         return;
     }
@@ -63,8 +67,8 @@ static void event_handler(const char *tag_id, char *buffer)
         char *innersaveptr;
         char *key = strtok_r(token, "=", &innersaveptr);
         char *value = strtok_r(NULL, "=", &innersaveptr);
-        if (key && value) 
-        {    
+        if (key && value)
+        {
             if (strcmp(key, "name") == 0) {
                 snprintf(device.name, sizeof(device.name), "%s", value);
             } else if (strcmp(key, "mac") == 0) {
@@ -77,7 +81,7 @@ static void event_handler(const char *tag_id, char *buffer)
                 snprintf(device.service, sizeof(device.service), "%s", value);
             }else if (strcmp(key, "appearance") == 0) {
                 snprintf(device.appearance, sizeof(device.appearance), "%s", value);
-            } 
+            }
             else if (strcmp(key, "connectable") == 0) {
                 device.connectable = atoi(value);
             }
@@ -86,8 +90,8 @@ static void event_handler(const char *tag_id, char *buffer)
         token = strtok_r(NULL, "|", &saveptr);
     }
 
-    if (internl_cb != NULL)
-        internl_cb("SCAN:UPDATE", &device);
+    if (internal_cb != NULL)
+        internal_cb("SCAN:UPDATE", &device);
 }
 
 uart_status_t bt_controller_init()
@@ -146,4 +150,107 @@ void bt_controller_select(const device_t *dev)
 void bt_controller_clean_ctx(void)
 {
     memset(&context.selected, 0, sizeof(context.selected));
+}
+
+static device_t *find_device(const char *mac)
+{
+    for (int index = 0; index < context.current_device_amount; index++)
+    {
+        if ( strcmp(context.devices[index].mac, mac) == 0)
+            return &context.devices[index];
+    }
+
+    return NULL;
+}
+
+void bt_reset_visible_devices(void)
+{
+    context.visible_device_amount = 0;
+
+    for (int i = 0; i < context.current_device_amount; i++) {
+        context.visible_devices[context.visible_device_amount++] = &context.devices[i];
+    }
+}
+
+void bt_apply_connectable_filter(void)
+{
+    int write_index = 0;
+
+    for (int i = 0; i < context.visible_device_amount; i++) {
+        device_t *device = context.visible_devices[i];
+
+        if (device->connectable) {
+            context.visible_devices[write_index++] = device;
+        }
+    }
+
+    context.visible_device_amount = write_index;
+}
+
+int bt_compare_device_ptrs_by_rssi_desc(const void *a, const void *b)
+{
+    const device_t *dev_a = *(const device_t **)a;
+    const device_t *dev_b = *(const device_t **)b;
+
+    if (dev_a->rssi < dev_b->rssi) return 1;
+    if (dev_a->rssi > dev_b->rssi) return -1;
+    return 0;
+}
+void bt_sort_visible_devices_by_nearest(void)
+{
+    if (context.visible_device_amount == 0)
+        bt_reset_visible_devices();
+        
+    qsort(
+        context.visible_devices,
+        context.visible_device_amount,
+        sizeof(device_t *),
+        bt_compare_device_ptrs_by_rssi_desc
+    );
+}
+
+int devices_length()
+{
+    return context.visible_device_amount;
+}
+
+device_t **bt_get_devices()
+{
+    return context.visible_devices;
+}
+
+void bt_controller_ctx_add_device(device_t *dev)
+{
+    if (!dev->mac[0])
+        return;
+
+    device_t *device = find_device(dev->mac);
+    if (device)
+    {
+        device->rssi = dev->rssi;
+        if (dev->name[0] && strcmp(device->name, DEFAULT_NAME) == 0)
+            snprintf(device->name, sizeof(device->name), "%s", dev->name);
+
+        if (dev->manufacturer[0] && strcmp(device->manufacturer, DEFAULT_NAME) == 0)
+            snprintf(device->manufacturer, sizeof(device->manufacturer), "%s", dev->manufacturer);
+
+        return;
+    }
+
+    if (context.current_device_amount >= MAX_DEVICES)
+    {
+        log_info("Can't save more devices, is in the limit of %d", MAX_DEVICES);
+        return;
+    }
+
+    device = &context.devices[context.current_device_amount++];
+
+    device->rssi = dev->rssi;
+    device->connectable = dev->connectable;
+
+    snprintf(device->name, sizeof(device->name), "%s", dev->name[0] ? dev->name : DEFAULT_NAME);
+    snprintf(device->mac, sizeof(device->mac), "%s", dev->mac);
+    snprintf(device->manufacturer, sizeof(device->manufacturer), "%s",  dev->manufacturer[0] ? dev->manufacturer : DEFAULT_NAME);
+    snprintf(device->service, sizeof(device->service), "%s",  dev->service[0] ? dev->service : DEFAULT_NAME);
+    snprintf(device->appearance, sizeof(device->appearance), "%s",  dev->appearance[0] ? dev->appearance : DEFAULT_NAME);
 }
--- page/bt/bt_controller.h
+++ page/bt/bt_controller.h
@@ -17,20 +17,27 @@ typedef struct {
     int connectable;
 } device_t;
 
-
-
 typedef struct bt_context_t bt_context_t;
-
 typedef void (*bt_event_cb)(const char *event, device_t *device);
 
 uart_status_t bt_controller_init(); // TODO: revisar si necesitamos mas datos
 uart_status_t start_scan();
 void set_scanner_cb(bt_event_cb new_callback);
 
+
+/** Bluetooth context function */
 bt_context_t *bt_controller_ctx(void);
 const device_t *selected_device(bt_context_t *ctx);
+void bt_controller_ctx_add_device(device_t *dev);
 void bt_controller_select(const device_t *dev);
 void bt_controller_clean_ctx(void);
+device_t **bt_get_devices();
+int devices_length();
+
+void bt_reset_visible_devices(void);
+void bt_apply_connectable_filter(void);
+int bt_compare_device_ptrs_by_rssi_desc(const void *a, const void *b);
+void bt_sort_visible_devices_by_nearest(void);
 
 #ifdef __cplusplus
 }
